80626811424932773498e1c9ee4bc3ad73e314ff
Karlven <karlven@outlook.com>
2021-04-28 01:03:27 +0800

    /

---
+++ hello/data_type.py
@@ -0,0 +1,14 @@
+str1 = '''一 23414
+滴42134
+水421344'''
+str2 = "wowowo"
+
+print(str1, str2)
+
+int1 = 1
+int2 = 2
+# actually int3 is a float
+int3 = 1 / 2
+
+print(int1, int2, int3)
+print(type(int1), type(int2), type(int3))
