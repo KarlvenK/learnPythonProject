a41dbe1f6d3c5b40040bb2a86efbe941fc9b421e
Karlven <karlven@outlook.com>
2021-05-18 23:37:32 +0800

    tuple

---
+++ list/tuple.py
@@ -0,0 +1,14 @@
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+#use for loop
+for dimension in dimensions:
+    print(dimension)
+
+print('modify dimensions')
+#modify tuple 
+dimensions = (1, 2)
+
+for dimension in dimensions:
+    print(dimension)
