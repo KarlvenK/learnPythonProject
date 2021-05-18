68898f9c22a53e86aeb702290319e79b843bfe01
Karlven <karlven@outlook.com>
2021-05-18 23:52:55 +0800

    if

---
+++ ctrl/if.py
@@ -0,0 +1,41 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+print()
+
+#number part
+answer = 17
+if answer != 42:
+    print('wrong answer')
+
+if (answer < 100) and (answer > 20) and (answer < 50 or answer > 20):
+    print('fuck')
+else:
+    print('shit')
+
+#check if A in list B
+objs = [1, 2, 3, 4, 5]
+
+if 5 in objs:
+    print('5 is in list')
+else:
+    print('5 is not in list')
+
+if 6 not in objs:
+    print('yeah')   
+else:
+    print('nope')
+
+#if-else-else structure
+age = 12
+
+if age < 4:
+    print('cost = 0')
+elif age < 18:
+    print('cost = 10')
+else:
+    print('cost = 15')
