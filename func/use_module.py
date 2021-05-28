2963baf4970cd257f0a12ff295137d6390a90146
Karlven <karlven@outlook.com>
2021-05-28 15:11:07 +0800

    /

--- func/use_module.py
+++ func/use_module.py
@@ -1,3 +1,13 @@
 import my_module
 
-my_module.make_pizza(16, 'pep')
+my_module.make_pizza(16, '1')
+
+import my_module as mp
+
+mp.make_pizza(1, '2')
+
+from my_module import make_pizza as mpp
+mpp(1, '3')
+
+from my_module import *
+my_module.make_pizza(1, '4')
